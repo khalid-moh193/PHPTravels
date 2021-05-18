aa63a1b1e8cb347370cd2f4775cac47f3f82dbf2
khalid.mohamed <khalid.mohamed@4sale.tech>
2021-05-18 22:47:57 +0200

    Add Home page class

--- src/main/java/Web/Pages/HomePage.java
+++ src/main/java/Web/Pages/HomePage.java
@@ -1,17 +1,38 @@
 package Web.Pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class HomePage extends Page {
-    public By searchField = By.name("q");
-
-
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
 
+    public By dropDownMenu = By.xpath("//div[@class='dropdown dropdown-login dropdown-tab']/a");
+    public By accountName = By.className("text-align-left");
+    public By logout = By.xpath("//a[normalize-space()='Logout']");
 
+
+    @Step("get account name text")
+    public String getAccountName(){
+        return getElementText(accountName);
+    }
+
+    @Step("- click on logout button")
+    public HomePage clickOnAccountDropDownMenu(){
+        clickElement(dropDownMenu);
+        return this;
+    }
+
+    @Step("- click on logout button")
+    public RegisterPage clickOnLogout(){
+        clickElement(logout);
+        return new RegisterPage(driver);
+    }
 }
